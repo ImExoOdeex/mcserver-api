4547b51f0e8869b796e23126e19ff8aa391f3271
ImExoOdeex <83508485+ImExoOdeex@users.noreply.github.com>
2022-10-15 23:38:06 +0100

    Delete build directory

--- build/splitAddress.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.splitAddress = void 0;
-var splitAddress = function (address, defaultPort) {
-    var _a = address.split(":"), host = _a[0], port = _a[1];
-    return { host: host, port: parseInt(port) || defaultPort || 25565 };
-};
-exports.splitAddress = splitAddress;
