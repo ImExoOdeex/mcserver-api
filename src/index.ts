79d62c2964870e6fbba42cf78436ccdb9bce395f
ImExoOdeex <odeex722@gmail.com>
2022-12-12 14:58:10 +0100

    Add response caching for 10 seconds

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from "express"
 import { getBedrockServerInfo } from "./getBedrockServerInfo"
 import { getServerInfo } from "./getServerInfo"
+const apicache = require('apicache')
 const cors = require('cors')
 
 const PORT = 8000
 const express = require('express')
 const app = express()
+let cache = apicache.middleware
+
+app.use(cache("10 seconds"))
 app.use(express.json())
 app.use(cors())
 
